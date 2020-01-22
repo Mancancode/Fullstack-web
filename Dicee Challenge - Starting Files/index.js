0ee7bad83eab72a6b679c5e18234c7ff767f460c
mancancode <madebymaniq@live.com>
2020-01-22 17:29:13 +0100

    completed Dice game

--- Dicee Challenge - Starting Files/index.js
+++
@@ -1,8 +0,0 @@
-var randomNumber1 = Math.floor(Math.random()*6) +1; //andm number between 1 & 6
-
-var randomdiceImage = "dice" + randomNumber1 + ".png" ;//dice.png1 -png6
-var randomimageSource ="images/" + randomdiceImage; // images/dice1.png - dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomimageSource );s
-
